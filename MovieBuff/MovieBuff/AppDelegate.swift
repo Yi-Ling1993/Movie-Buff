8fcac341cd1d9ffb45bd43ae8eba724e32c96d2e
Liu Yi-Ling <liuyiling1993@gmail.com>
2018-09-19 13:53:44 +0800

    Add SwiftLint

---
+++ MovieBuff/MovieBuff/AppDelegate.swift
@@ -0,0 +1,41 @@
+//
+//  AppDelegate.swift
+//  MovieBuff
+//
+//  Created by 劉奕伶 on 2018/9/19.
+//  Copyright © 2018年 Appwork School. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+       
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+       
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        
+    }
+
+}
