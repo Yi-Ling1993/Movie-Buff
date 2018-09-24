753d04d1cbe98633e2263bd0edb03bef20e265ba
Liu Yi-Ling <liuyiling1993@gmail.com>
2018-09-24 23:14:05 +0800

    Adjust table view

--- MovieBuff/MovieBuff/TheaterDetailViewController.swift
+++ MovieBuff/MovieBuff/TheaterDetailViewController.swift
@@ -38,6 +38,6 @@ extension TheaterDetailViewController: UITableViewDelegate, UITableViewDataSourc
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 80
+        return 100
     }
 }
