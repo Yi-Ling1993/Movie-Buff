0975c5cab595e65259d7141d434514a2816e8f9e
Liu Yi-Ling <liuyiling1993@gmail.com>
2018-09-24 23:11:01 +0800

    Finish table view UI in TheaterDetail

--- MovieBuff/MovieBuff/ShowtimeTableViewCell.swift
+++ MovieBuff/MovieBuff/ShowtimeTableViewCell.swift
@@ -9,10 +9,21 @@
 import UIKit
 
 class ShowtimeTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var backGroundView: UIView!
 
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+
+        
+        backGroundView.backgroundColor = UIColor(red: 77/255, green: 75/255, blue: 75/255, alpha: 1)
+        backGroundView.layer.cornerRadius = 2
+        backGroundView.layer.masksToBounds = false
+        
+        backGroundView.layer.shadowColor = UIColor.white.withAlphaComponent(0.5).cgColor
+        
+        backGroundView.layer.shadowOffset = CGSize(width: 0, height: 0)
+        backGroundView.layer.shadowOpacity = 0.8
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
