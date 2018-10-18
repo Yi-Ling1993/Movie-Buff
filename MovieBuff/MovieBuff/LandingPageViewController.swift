4ddf04a27c8515df540374d856d4be39f86f75c6
Liu Yi-Ling <liuyiling1993@gmail.com>
2018-10-18 11:12:51 +0800

    Set landing page

---
+++ MovieBuff/MovieBuff/LandingPageViewController.swift
@@ -0,0 +1,42 @@
+//
+//  LandingPageViewController.swift
+//  Taipei Movies
+//
+//  Created by 劉奕伶 on 2018/10/18.
+//  Copyright © 2018年 Appwork School. All rights reserved.
+//
+
+import UIKit
+
+class LandingPageViewController: UIViewController {
+    
+    @IBAction func toInTheater(_ sender: UIButton) {
+    }
+    
+    @IBAction func toThisWeek(_ sender: UIButton) {
+    }
+    
+    @IBAction func toSoon(_ sender: UIButton) {
+    }
+    
+    @IBAction func toTheater(_ sender: UIButton) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
