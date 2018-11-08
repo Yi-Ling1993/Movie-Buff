480c1e82d5a2e94657fabb5a1e3983423c230cd9
Liu Yi-Ling <liuyiling1993@gmail.com>
2018-11-08 09:15:18 +0800

    ready for merge to master

--- MovieBuff/MovieBuff/TheaterTableViewCell.swift
+++ MovieBuff/MovieBuff/TheaterTableViewCell.swift
@@ -11,7 +11,6 @@ import UIKit
 class TheaterTableViewCell: UITableViewCell {
 
     @IBOutlet weak var backGroundCardView: UIView!
-    
     @IBOutlet weak var theaterName: UILabel!
     @IBOutlet weak var addressLabel: UILabel!
     @IBOutlet weak var pinButton: UIButton!
